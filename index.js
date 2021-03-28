4618ec870628720ac875bfa8a207f1e0ba1780a1
David P Sanders <davepaulsanders@gmail.com>
2021-03-28 15:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function iLoveTheBeatles(number) {
 }
 
 function johnLennonFacts(facts){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", ];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", "While on tour, he enjoyed playing Monopoly",
+  "Harmonica was the first instrument Lennon learned to play"];
   var newFacts = [];
   var i = 0
   while (i < facts.length) {
