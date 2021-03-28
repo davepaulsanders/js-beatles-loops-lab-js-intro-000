604f066f5c06b56a3f4c3d67912ab0769fdabafa
David P Sanders <davepaulsanders@gmail.com>
2021-03-28 14:47:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+
+
+function theBeatlesPlay(musicians, instruments){
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+  var roles = []
+  for(let i = 0, len = musicians.length; i < len; i++) {
+    roles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+   return roles;
+}
+
+function johnLennonFacts() {
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice",
+  "While on tour, he enjoyed playing Monopoly",
+  "Harmonica was the first instrument Lennon learned to play"
+]
+var excitingFacts = []
+let i = 0
+while([i <= facts.length]){
+        excitingFacts.push(`${facts[i]}!!!`)
+        i = i++
+      }
+return excitingFacts
+}
