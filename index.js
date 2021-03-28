ae9ba583386913d026044d2efe19492a40422adf
David P Sanders <davepaulsanders@gmail.com>
2021-03-28 15:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,23 +12,17 @@ function theBeatlesPlay(musicians, instruments){
    return roles;
 }
 
-function johnLennonFacts() {
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice",
-  
-]
 
-let i = 0
-var newFacts = []
-while(i < facts.length){
-      newFacts.push(facts[i] + '!!!')
-        i++
-      }
-return newFacts
-}
 
 function iLoveTheBeatles(number) {
 }
+
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return newFacts;
